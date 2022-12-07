dd4b3315ac798760476181b08610b790b19c7124
defeef <97907305+defeef@users.noreply.github.com>
2022-12-07 12:24:14 -0500

    Create AFile.java

---
+++ AFile.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class AFile {
+    private String name;
+    private int size;
+    
+    public AFile(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+}
